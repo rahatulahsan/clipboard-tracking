d73ff5b5178656951b1c1ec792577e1c6611dd5a
rahatulahsan <rahatulahsan@gmail.com>
2023-08-09 15:20:02 +0600

    Dark mode integration Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['./pages/**/*.{html,js}',
   './components/**/*.{html,js}',,
   "./src/**/*.{html,js}",
